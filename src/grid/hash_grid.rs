8c9c136e8a8ded7010b7a8f97ae1712c67e344f2
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-11-29 17:05:36 +0100

    add bounds test for hashgrid

--- src/grid/hash_grid.rs
+++ src/grid/hash_grid.rs
@@ -25,17 +25,21 @@ impl<'a, T> HashGrid<'a, T> {
         }
     }
 
-    fn update_bounds(&mut self, point: &Point) {
-        if self.grid.is_empty() {
-            self.upper = point.to_owned();
-            self.lower = point.to_owned();
+    pub fn with_bounds(lower: Point, upper: Point) -> HashGrid<'a, T> {
+        HashGrid {
+            grid: HashMap::new(),
+            lower,
+            upper,
+            phantom: PhantomData,
         }
+    }
 
+    fn update_bounds(&mut self, point: &Point) {
         self.upper.x = self.upper.x.max(point.x);
         self.upper.y = self.upper.y.max(point.y);
 
-        self.lower.x = self.upper.x.min(point.x);
-        self.lower.y = self.upper.y.min(point.y);
+        self.lower.x = self.lower.x.min(point.x);
+        self.lower.y = self.lower.y.min(point.y);
     }
 }
 
@@ -73,5 +77,44 @@ impl<'a, T> Grid<'a> for HashGrid<'a, T> {
 
     fn entries(&'a self) -> impl Iterator<Item = (Point, Self::ReturnItem)> {
         self.grid.iter().map(|(k, v)| (k.to_owned(), v))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    fn bounds_should_update() {
+        let mut grid = HashGrid::new();
+
+        assert_eq!(grid.bounds(), (Point::new(0, 0), Point::new(0, 0)));
+
+        grid.set(&Point::new(1, 1), ());
+        assert_eq!(grid.bounds(), (Point::new(0, 0), Point::new(1, 1)));
+
+        grid.set(&Point::new(5, 1), ());
+        assert_eq!(grid.bounds(), (Point::new(0, 0), Point::new(5, 1)));
+
+        grid.set(&Point::new(2, 8), ());
+        assert_eq!(grid.bounds(), (Point::new(0, 0), Point::new(5, 8)));
+
+        grid.set(&Point::new(-2, -2), ());
+        assert_eq!(grid.bounds(), (Point::new(-2, -2), Point::new(5, 8)));
+    }
+
+    #[rstest]
+    fn should_keep_default_bounds() {
+        let mut grid = HashGrid::with_bounds(Point::new(-5, -5), Point::new(5, 5));
+
+        assert_eq!(grid.bounds(), (Point::new(-5, -5), Point::new(5, 5)));
+
+        grid.set(&Point::new(1, 1), ());
+        assert_eq!(grid.bounds(), (Point::new(-5, -5), Point::new(5, 5)));
+
+        grid.set(&Point::new(10, 10), ());
+        assert_eq!(grid.bounds(), (Point::new(-5, -5), Point::new(10, 10)));
     }
 }
