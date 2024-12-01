f04020a0e07fa2f1999215d25c8f6637833a8eed
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-01 09:20:40 +0100

    solve d1p2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::collections::{hash_map::Entry, HashMap};
+
 use itertools::Itertools;
 
 advent_of_code::solution!(1);
@@ -33,7 +35,44 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let input = input
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .collect_vec();
+
+    // MARKER .sum() iterator functions would be nice
+    let left = input
+        .iter()
+        .map(|a| a.0)
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect_vec();
+    let right = input
+        .iter()
+        .map(|a| a.1)
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect_vec();
+
+    let mut map = HashMap::new();
+    for n in right {
+        match map.entry(n) {
+            Entry::Vacant(entry) => {
+                entry.insert(1);
+            }
+            Entry::Occupied(mut entry) => {
+                entry.insert(entry.get() + 1);
+            }
+        }
+    }
+
+    let result = left
+        .into_iter()
+        .map(|number| {
+            let count = map.get(&number).unwrap_or(&0);
+            number * count
+        })
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -49,6 +88,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
