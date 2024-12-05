9becb3631413449bc983b3ae04a1397d87465ebb
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-05 08:49:22 +0100

    solve d5p2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use advent_of_code::AocItertools;
 use itertools::Itertools;
 
@@ -42,7 +44,56 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (ordering, updates) = input.split_once("\n\n").unwrap();
+    let ordering = ordering
+        .lines()
+        .map(|l| {
+            let (left, right) = l.split_once("|").unwrap();
+
+            (left.parse::<u32>().unwrap(), right.parse::<u32>().unwrap())
+        })
+        .collect_vec();
+
+    let mut updates = updates
+        .lines()
+        .map(|l| l.split(",").u32().collect_vec())
+        .filter(|update| {
+            let iter = update.iter().zip(update.iter().skip(1));
+
+            for (left, right) in iter {
+                if !ordering.iter().any(|ord| *ord == (*left, *right)) {
+                    return true;
+                }
+            }
+
+            false
+        })
+        .collect_vec();
+
+    for update in updates.iter_mut() {
+        update.sort_by(|a, b| {
+            let s = ordering
+                .iter()
+                .find(|ord| **ord == (*a, *b) || **ord == (*b, *a))
+                .unwrap();
+
+            match s {
+                _ord if *s == (*a, *b) => Ordering::Less,
+                _ord if *s == (*b, *a) => Ordering::Greater,
+                _ => panic!("{}{}, {:?}", a, b, s),
+            }
+        });
+    }
+
+    let result = updates
+        .iter()
+        .map(|v| {
+            let middle = (v.len() - 1) / 2;
+            v.get(middle).unwrap()
+        })
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -58,6 +109,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
