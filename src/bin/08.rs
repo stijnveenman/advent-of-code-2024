aa790cfe101a863321497722e88428c1881faba8
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-08 11:22:35 +0100

    solve d8p2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -33,7 +33,40 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid = CharGrid::new(input);
+
+    let mut groups = grid.entries().grouped_by(|(_p, c)| *c);
+    groups.remove(&'.');
+
+    let mut set = HashSet::new();
+
+    for (_, v) in groups {
+        for v in v.iter().permutations(2) {
+            let (p1, _) = v.first().unwrap();
+            let (p2, _) = v.get(1).unwrap();
+
+            // let b1 = *p1 + (*p1 - *p2);
+            let dir = *p1 - *p2;
+            set.insert(*p2);
+
+            let mut b = *p1 + dir;
+            while grid.in_bounds(&b) {
+                set.insert(b);
+
+                b += dir;
+            }
+        }
+    }
+
+    // let g = grid.draw(|p, c| {
+    //     if set.contains(p) {
+    //         return "#".into();
+    //     }
+    //     c.unwrap().into()
+    // });
+    // println!("{}", g);
+
+    Some(set.len() as u32)
 }
 
 #[cfg(test)]
