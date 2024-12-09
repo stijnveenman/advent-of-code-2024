a872a093e46b4805b3651f549fe486074644a0bc
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-09 09:42:25 +0100

    running but invalid solution

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::usize;
 
 use itertools::{repeat_n, Itertools};
@@ -74,8 +75,100 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(checksum(&input[..len]))
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+#[derive(Debug)]
+enum Block {
+    /// (length of the block)
+    Free(u32),
+    /// (length of the block, file index)
+    Used(u32, u32),
+}
+
+fn expand_blocks(input: &str) -> Vec<Block> {
+    let mut file_index = 0u32;
+    let mut is_file = true;
+    let mut v = Vec::new();
+
+    for c in input.chars() {
+        let length = c.to_digit(10).unwrap();
+
+        let item = match is_file {
+            true => {
+                let block = Block::Used(length, file_index);
+                file_index += 1;
+                block
+            }
+            false => Block::Free(length),
+        };
+
+        is_file = !is_file;
+
+        if length != 0 {
+            v.push(item);
+        }
+    }
+
+    v
+}
+
+fn compress_blocks(input: &mut Vec<Block>) {
+    let mut tail = input.len() - 1;
+
+    while tail > 0 {
+        let (length, idx) = match input[tail] {
+            Block::Free(_) => {
+                tail -= 1;
+                continue;
+            }
+            Block::Used(length, idx) => (length, idx),
+        };
+
+        let Some((free_index, free_block)) = input[..tail].iter().find_position(|i| match i {
+            Block::Used(_, _) => false,
+            Block::Free(len) => *len >= length,
+        }) else {
+            tail -= 1;
+            continue;
+        };
+
+        let Block::Free(free_len) = free_block else {
+            panic!("should have free block here");
+        };
+
+        input[free_index] = Block::Free(*free_len - length);
+        input[tail] = Block::Free(length);
+
+        input.insert(free_index, Block::Used(length, idx));
+
+        tail -= 1;
+    }
+}
+
+fn blocks_checksum(blocks: &[Block]) -> usize {
+    let mut idx = 0usize;
+    let mut checksum = 0usize;
+
+    for block in blocks {
+        let (length, block_index) = match block {
+            Block::Free(length) => {
+                idx += *length as usize;
+                continue;
+            }
+            Block::Used(len, idx) => (len, idx),
+        };
+
+        for _ in 0..*length {
+            checksum += idx * (*block_index as usize);
+            idx += 1;
+        }
+    }
+    checksum
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut blocks = expand_blocks(input.trim());
+    compress_blocks(&mut blocks);
+
+    Some(blocks_checksum(&blocks))
 }
 
 #[cfg(test)]
@@ -91,6 +184,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2858));
     }
 }
