a4b32b7af6f491426f5dbbe36d5696b8efe105b5
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-11 13:15:53 +0100

    solve d11p1

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,14 +1,69 @@
 use advent_of_code::AocItertools;
 use itertools::Itertools;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 advent_of_code::solution!(11);
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn num_length(mut num: usize) -> usize {
+    let mut len = 0;
+
+    while num > 0 {
+        num /= 10;
+        len += 1;
+    }
+
+    len
+}
+
+fn take_top(mut num: usize, len: usize) -> usize {
+    for _ in 0..len {
+        num /= 10;
+    }
+
+    num
+}
+
+fn take_bottom(num: usize, len: usize) -> usize {
+    let modulo = 10usize.pow(len as u32);
+
+    num % modulo
+}
+
+// we could return a [usize; 2] and filter out the second stone if it didn't split
+// or we could limit a single map stone to a certain size, if it gets above this sive
+// we split resursively into a second call
+//
+// For now, use a memory inefficient way
+fn map_stones(stones: &[usize]) -> Vec<usize> {
+    stones
+        .iter()
+        .flat_map(|stone| match stone {
+            0 => vec![1],
+            s if num_length(*s) % 2 == 0 => vec![
+                take_top(*s, num_length(*s) / 2),
+                take_bottom(*s, num_length(*s) / 2),
+            ],
+            s => vec![*s * 2024], // s * 2024
+        })
+        .collect_vec()
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
     let stones = input.trim().split(" ").usize().collect_vec();
 
-    dbg!(stones);
+    let result = stones
+        .par_iter()
+        .map(|stone| {
+            let mut stones = vec![*stone];
+            for _ in 0..25 {
+                stones = map_stones(&stones);
+            }
 
-    None
+            stones.len()
+        })
+        .sum();
+
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
