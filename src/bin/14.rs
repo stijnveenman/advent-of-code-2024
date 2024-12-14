16c439e654e1e65aca377f96b0d2fd41b5fca7ec
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-14 13:28:17 +0100

    solve d14p2

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,5 +1,3 @@
-use std::usize;
-
 use advent_of_code::{components::Point, AocItertools};
 use itertools::Itertools;
 
@@ -50,7 +48,84 @@ pub fn part_one(input: &str) -> Option<usize> {
     solve_part_one(input, Point::new(101, 103))
 }
 
+#[allow(dead_code)]
+fn draw(upper: Point, v: &[(Point, Point)]) -> String {
+    let mut s = String::new();
+
+    for y in 0..=upper.y {
+        for x in 0..=upper.x {
+            let point = Point::new(x, y);
+            let count = v.iter().filter(|(a, _)| a == &point).count();
+
+            if count == 0 {
+                s += " ";
+            } else {
+                s += "█";
+            };
+        }
+
+        if y != upper.y {
+            s += "\n";
+        }
+    }
+
+    println!("{}\n", s);
+    s
+}
+
+fn has(v: &[(Point, Point)], p: &Point) -> bool {
+    v.iter().any(|(r, _)| r == p)
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
+    let size = Point::new(101, 103);
+
+    let mut robots = input
+        .lines()
+        .map(|l| {
+            let (p, v) = l.split_once(" ").unwrap();
+            let (_, p) = p.split_once("=").unwrap();
+            let (_, v) = v.split_once("=").unwrap();
+
+            (
+                Point::parse_seperated(p, ",").unwrap(),
+                Point::parse_seperated(v, ",").unwrap(),
+            )
+        })
+        .collect_vec();
+
+    for i in 1..100_000 {
+        robots = robots
+            .into_iter()
+            .map(|(p, v)| {
+                let pos = p + (v);
+
+                (Point::new(pos.x % size.x, pos.y % size.y), v)
+            })
+            .map(|(p, v)| {
+                let x = if p.x < 0 { size.x + p.x } else { p.x };
+                let y = if p.y < 0 { size.y + p.y } else { p.y };
+
+                (Point::new(x, y), v)
+            })
+            .collect_vec();
+
+        let has_tree = robots.iter().any(|(p, _)| {
+            for y in 1..5 {
+                if !(-y..y).all(|x| has(&robots, &Point::new(p.x + x, p.y + y))) {
+                    return false;
+                }
+            }
+
+            true
+        });
+
+        if has_tree {
+            // draw(size, &robots);
+            return Some(i as u32);
+        }
+    }
+
     None
 }
 
