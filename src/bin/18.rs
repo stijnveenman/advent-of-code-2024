dd0e5dc421bc2518f6eabd326b893e58740e729c
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-18 08:27:33 +0100

    solve d18p2

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -20,10 +20,10 @@ fn solve_part1(input: &str, size: Point, take: usize) -> Option<usize> {
         grid.set(p, true);
     }
 
-    grid.print(|_p, c| match c {
-        Some(true) => "#".into(),
-        _ => ".".into(),
-    });
+    // grid.print(|_p, c| match c {
+    //     Some(true) => "#".into(),
+    //     _ => ".".into(),
+    // });
 
     dijkstra(&grid, Point::new(0, 0), size, |_p, v| match v {
         Some(true) => None,
@@ -35,8 +35,44 @@ pub fn part_one(input: &str) -> Option<usize> {
     solve_part1(input, Point::new(70, 70), 1024)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn try_for(points: &[Point], take: usize) -> bool {
+    let mut grid = HashGrid::with_bounds(Point::new(0, 0), Point::new(70, 70));
+
+    for p in points.iter().take(take) {
+        grid.set(p, true);
+    }
+
+    dijkstra(&grid, Point::new(0, 0), grid.bounds().1, |_p, v| match v {
+        Some(true) => None,
+        _ => Some(1),
+    })
+    .is_some()
+}
+
+pub fn part_two(input: &str) -> Option<String> {
+    let points = input
+        .lines()
+        .map(|l| Point::parse_seperated(l, ",").unwrap())
+        .collect_vec();
+
+    // at the beginning, we know 1024 (input) works, and the end shouldn't work
+    let mut min = 1024;
+    let mut max = points.len();
+
+    // Binary search the first point that fails
+    while min + 1 < max {
+        let current = min + (max - min) / 2;
+
+        if try_for(&points, current) {
+            // if it succeeds, take the right
+            min = current;
+        } else {
+            // if it fails, take the left
+            max = current;
+        }
+    }
+
+    Some(points[min].to_string())
 }
 
 #[cfg(test)]
@@ -53,9 +89,9 @@ mod tests {
         assert_eq!(result, Some(22));
     }
 
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
+    // #[test]
+    // fn test_part_two() {
+    //     let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+    //     assert_eq!(result, None);
+    // }
 }
