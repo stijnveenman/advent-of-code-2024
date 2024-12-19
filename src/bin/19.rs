1a9347b79de66eec38cb6b868bd845d4f9375387
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-19 08:56:25 +0100

    solve d19p1

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,11 +1,39 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use advent_of_code::AocItertools;
 use itertools::Itertools;
 
 advent_of_code::solution!(19);
 
-fn find_match(pattern: &str, options: &HashMap<char, Vec<&str>>) -> bool {
+fn find_match(
+    pattern: &str,
+    options: &HashMap<char, Vec<&str>>,
+    cache: &mut HashSet<usize>,
+) -> bool {
+    if cache.contains(&pattern.len()) {
+        return false;
+    }
+
+    let Some(c) = pattern.chars().next() else {
+        return true;
+    };
+
+    let Some(option) = options.get(&c) else {
+        cache.insert(pattern.len());
+        return false;
+    };
+
+    for o in option.iter().filter(|o| pattern.starts_with(*o)) {
+        if pattern.len() == o.len() {
+            return true;
+        }
+
+        if find_match(&pattern[o.len()..], options, cache) {
+            return true;
+        }
+    }
+
+    cache.insert(pattern.len());
     false
 }
 
@@ -20,8 +48,7 @@ pub fn part_one(input: &str) -> Option<usize> {
 
     let result = patterns
         .into_iter()
-        .filter(|p| find_match(p, &options))
-        .dbg()
+        .filter(|p| find_match(p, &options, &mut HashSet::new()))
         .count();
 
     Some(result)
