d7659d5f017a9d97d798a2e050a3c58af6ceacf4
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-19 15:23:04 +0100

    solve d19p2

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use advent_of_code::AocItertools;
 use itertools::Itertools;
@@ -8,33 +8,35 @@ advent_of_code::solution!(19);
 fn find_match(
     pattern: &str,
     options: &HashMap<char, Vec<&str>>,
-    cache: &mut HashSet<usize>,
-) -> bool {
-    if cache.contains(&pattern.len()) {
-        return false;
+    cache: &mut HashMap<usize, usize>,
+) -> usize {
+    if let Some(current) = cache.get(&pattern.len()) {
+        return *current;
     }
 
     let Some(c) = pattern.chars().next() else {
-        return true;
+        return 1;
     };
 
     let Some(option) = options.get(&c) else {
-        cache.insert(pattern.len());
-        return false;
+        cache.insert(pattern.len(), 0);
+        return 0;
     };
 
-    for o in option.iter().filter(|o| pattern.starts_with(*o)) {
-        if pattern.len() == o.len() {
-            return true;
-        }
+    let value = option
+        .iter()
+        .filter(|o| pattern.starts_with(*o))
+        .map(|o| {
+            if pattern.len() == o.len() {
+                return 1;
+            }
 
-        if find_match(&pattern[o.len()..], options, cache) {
-            return true;
-        }
-    }
+            find_match(&pattern[o.len()..], options, cache)
+        })
+        .sum();
 
-    cache.insert(pattern.len());
-    false
+    cache.insert(pattern.len(), value);
+    value
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
@@ -48,14 +50,27 @@ pub fn part_one(input: &str) -> Option<usize> {
 
     let result = patterns
         .into_iter()
-        .filter(|p| find_match(p, &options, &mut HashSet::new()))
+        .filter(|p| find_match(p, &options, &mut HashMap::new()) > 0)
         .count();
 
     Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let (options, patterns) = input.split_once("\n\n").unwrap();
+    let options = options
+        .split(",")
+        .map(|p| p.trim())
+        .grouped_by(|c| c.chars().next().unwrap());
+
+    let patterns = patterns.lines().collect_vec();
+
+    let result = patterns
+        .into_iter()
+        .map(|p| find_match(p, &options, &mut HashMap::new()))
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -71,6 +86,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(16));
     }
 }
