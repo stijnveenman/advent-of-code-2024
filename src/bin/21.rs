1e97c0581f77f71f597808ebd292305f7ca2fcf8
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-22 11:51:32 +0100

    change code structure

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 use advent_of_code::{
     components::Point,
     grid::{hash_grid::HashGrid, Grid},
@@ -7,47 +9,57 @@ use itertools::{repeat_n, Itertools};
 advent_of_code::solution!(21);
 
 type Pad = HashGrid<'static, char>;
-fn numpad() -> Pad {
-    let mut grid = HashGrid::new();
+fn numpad(c: char) -> Point {
+    match c {
+        '7' => Point::new(0, 0),
+        '8' => Point::new(1, 0),
+        '9' => Point::new(2, 0),
 
-    grid.set(&Point::new(0, 0), '7');
-    grid.set(&Point::new(1, 0), '8');
-    grid.set(&Point::new(2, 0), '9');
+        '4' => Point::new(0, 1),
+        '5' => Point::new(1, 1),
+        '6' => Point::new(2, 1),
 
-    grid.set(&Point::new(0, 1), '4');
-    grid.set(&Point::new(1, 1), '5');
-    grid.set(&Point::new(2, 1), '6');
+        '1' => Point::new(0, 2),
+        '2' => Point::new(1, 2),
+        '3' => Point::new(2, 2),
 
-    grid.set(&Point::new(0, 2), '1');
-    grid.set(&Point::new(1, 2), '2');
-    grid.set(&Point::new(2, 2), '3');
+        '0' => Point::new(1, 3),
+        'A' => Point::new(2, 3),
 
-    grid.set(&Point::new(1, 3), '0');
-    grid.set(&Point::new(2, 3), 'A');
-
-    grid
+        c => panic!("not handled: {}", c),
+    }
 }
 
-fn direction_keypad() -> Pad {
-    let mut grid = HashGrid::new();
+fn direction_keypad(c: char) -> Point {
+    match c {
+        '^' => Point::new(1, 0),
+        'A' => Point::new(2, 0),
 
-    grid.set(&Point::new(1, 0), '^');
-    grid.set(&Point::new(2, 0), 'A');
+        '<' => Point::new(0, 1),
+        'v' => Point::new(1, 1),
+        '>' => Point::new(2, 1),
 
-    grid.set(&Point::new(0, 1), '<');
-    grid.set(&Point::new(1, 1), 'V');
-    grid.set(&Point::new(2, 1), '>');
-
-    grid
+        c => panic!("not handled: {}", c),
+    }
 }
 
-fn move_pad(pad: &Pad, from: char, to: char) -> Vec<char> {
+fn map_pad(c: char, index: usize) -> Point {
+    if index == 0 {
+        numpad(c)
+    } else {
+        direction_keypad(c)
+    }
+}
+
+const ROBOT_COUNT: usize = 3;
+fn move_pad(robots: &mut [char; ROBOT_COUNT], index: usize, to: char) -> Vec<char> {
+    let from = robots[index];
     if from == to {
-        // i think sometimes we might need A here
         return vec!['A'];
     }
-    let current = pad.find_by_value(&from).unwrap();
-    let end = pad.find_by_value(&to).unwrap();
+
+    let current = map_pad(from, index);
+    let end = map_pad(to, index);
 
     let diff = end - current;
 
@@ -58,44 +70,32 @@ fn move_pad(pad: &Pad, from: char, to: char) -> Vec<char> {
 
     let mut path = repeat_n('>', right.unsigned_abs())
         .chain(repeat_n('^', up.unsigned_abs()))
-        .chain(repeat_n('V', down.unsigned_abs()))
+        .chain(repeat_n('v', down.unsigned_abs()))
         .chain(repeat_n('<', left.unsigned_abs()))
         .collect_vec();
 
     path.push('A');
+    robots[index] = to;
     path
 }
 
-struct Robot {
-    pos: char,
-    pad: Pad,
-}
-
-impl Robot {
-    fn new(pad: Pad) -> Robot {
-        Robot { pos: 'A', pad }
-    }
-
-    fn press(&mut self, to: char) -> Vec<char> {
-        let path = move_pad(&self.pad, self.pos, to);
-        self.pos = to;
-        path
-    }
-}
-
 fn solve_number(number: &str) -> usize {
-    let mut r1 = Robot::new(numpad());
-    let mut r2 = Robot::new(direction_keypad());
-    let mut r3 = Robot::new(direction_keypad());
+    let mut robots = ['A'; ROBOT_COUNT];
 
     let path = number
         .chars()
         .flat_map(|c| {
-            let path = r1.press(c);
+            let path = move_pad(&mut robots, 0, c);
 
-            let path = path.into_iter().flat_map(|c| r2.press(c)).collect_vec();
+            let path = path
+                .into_iter()
+                .flat_map(|c| move_pad(&mut robots, 1, c))
+                .collect_vec();
 
-            let path = path.into_iter().flat_map(|c| r3.press(c)).collect_vec();
+            let path = path
+                .into_iter()
+                .flat_map(|c| move_pad(&mut robots, 2, c))
+                .collect_vec();
             // dbg!(&path);
             path
         })
