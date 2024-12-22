643fcf1e19359c5d63015e461bf9f223935a251b
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-12-22 13:44:14 +0100

    solve d22p1

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -3,12 +3,43 @@ use itertools::Itertools;
 
 advent_of_code::solution!(22);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let input = input.lines().u32().collect_vec();
+fn mix(value: usize, number: usize) -> usize {
+    value ^ number
+}
 
-    dbg!(input);
+fn prune(number: usize) -> usize {
+    number % 16777216
+}
 
-    None
+fn evolve(mut number: usize) -> usize {
+    number = mix(number * 64, number);
+    number = prune(number);
+
+    number = mix(number / 32, number);
+    number = prune(number);
+
+    number = mix(number * 2048, number);
+    number = prune(number);
+
+    number
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let input = input.lines().usize().collect_vec();
+
+    let result = input
+        .into_iter()
+        .map(|secret| {
+            let mut current = secret;
+            for _ in 0..2000 {
+                current = evolve(current);
+            }
+
+            current
+        })
+        .sum();
+
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -29,5 +60,17 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_mix() {
+        let result = mix(42, 15);
+        assert_eq!(result, 37);
+    }
+
+    #[test]
+    fn test_prune() {
+        let result = prune(100000000);
+        assert_eq!(result, 16113920);
     }
 }
